9214fb51f6301e22f59fb8337a81d890a402d3fb
IP777 <ivanov_piter@ukr.net>
2020-04-17 20:26:52 +0300

    Updates

---
+++ precache-manifest.6bb25c2bf1a215c2b973a470022880cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820ae99051fb4bf2fb725722cd706572",
+    "url": "/IP777/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "10af40262a66edc91e17",
+    "url": "/IP777/goit-react-hw-03-image-finder/static/css/main.57f4c867.chunk.css"
+  },
+  {
+    "revision": "a598c7f958d19a3e8a64",
+    "url": "/IP777/goit-react-hw-03-image-finder/static/js/2.52ae43da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IP777/goit-react-hw-03-image-finder/static/js/2.52ae43da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10af40262a66edc91e17",
+    "url": "/IP777/goit-react-hw-03-image-finder/static/js/main.75d04492.chunk.js"
+  },
+  {
+    "revision": "a1433275def3abb114c6",
+    "url": "/IP777/goit-react-hw-03-image-finder/static/js/runtime-main.07483bfb.js"
+  }
+]);
